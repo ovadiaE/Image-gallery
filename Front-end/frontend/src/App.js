6c2ac46c7c62cb4f8cf3bf94bcb945ae82ab8163
Ovadia Esquenazi <ovadia.esq@gmail.com>
2021-09-10 18:17:08 +0300

    first api call

--- Front-end/frontend/src/App.js
+++ Front-end/frontend/src/App.js
@@ -1,11 +1,32 @@
+import { useState } from "react";
+import axios from "axios";
 import "bootstrap/dist/css/bootstrap.min.css";
 import Header from "./components/Header/Header";
 import Search from "./components/Search/Search";
+
 function App() {
+  const [search, setSearch] = useState("");
+  const UNSPLASH_KEY = process.env.REACT_APP_UNSPLASH_KEY;
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      let response = await axios.get(
+        `https://api.unsplash.com/photos/random/?query=${search}&client_id=${UNSPLASH_KEY}`
+      );
+      console.log(response.data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   return (
     <div>
       <Header />
-      <Search />
+      <Search
+        handleSubmit={handleSubmit}
+        search={search}
+        setSearch={setSearch}
+      />
     </div>
   );
 }
--- Front-end/frontend/src/components/Search/Search.js
+++ Front-end/frontend/src/components/Search/Search.js
@@ -1,12 +1,18 @@
 import React from "react";
 import "./Search.css";
 import { Container, Row, Col, Form, Button } from "react-bootstrap";
-const Search = () => {
+
+const Search = ({ search, setSearch, handleSubmit }) => {
   return (
     <Container className="mt-4">
       <Row className="justify-content-center">
         <Col xs={12} md={8}>
-          <Form>
+          <Form
+            type="text"
+            value={search}
+            onChange={(e) => setSearch(e.target.value)}
+            onSubmit={handleSubmit}
+          >
             <Row>
               <Col xs={9}>
                 <Form.Control placeholder="Search Image" />
