eab6a541bcb66b94767e7be09841075fe86c24be
Ovadia Esquenazi <ovadia.esq@gmail.com>
2021-09-14 16:26:02 +0300

    unfixed 401 on get

--- backend/app.py
+++ backend/app.py
@@ -1,9 +1,23 @@
-from flask import Flask
+import requests
+from flask import Flask, request
+
+UNSPLASH_URL = "https://api.unsplash.com/photos/random/"
+UNSPLASH_KEY =  "2yfY1om1BnxEGUlr4qwqqvU_avqkb1SjjBp-7dgstJ8"
+                
 
 app = Flask(__name__)
 
-@app.route("/hello")
-def hello_world():
-    return "<p>did it work?</p>"
+
+@app.route("/new-image")
+def new_image():
+    word = request.args.get("query")
+    headers = {"Authorization": "client-id" + UNSPLASH_KEY}
+    print(UNSPLASH_KEY)
+    params = {"query": word}
+    response = requests.get(url=UNSPLASH_URL,headers=headers,params=params)
+    print(response) 
+    print(response.text)
+    return(response.text)
+    
 if __name__ == "__main__":
-    app.run (host ="0.0.0.0", port =5050)
+       app.run(host="0.0.0.0", port=5050)
